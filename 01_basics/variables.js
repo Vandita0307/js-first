5379ea2c967b56610f0b1d26e4dfa4425532d0cd
Vandita0307 <parashervandita@gmail.com>
2026-06-07 18:44:57 +0000

    container config

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountID=12458
+let accountEmail="pari@abc.com"
+var accountPassword="123"
+accountCity="Jaipur"
+//accountID=10 //not allowed
+console.log(accountID);
+ console.table([accountID,accountEmail,accountPassword,accountCity])
+ /*
+ prefer not using var because of functional scope
+ */
