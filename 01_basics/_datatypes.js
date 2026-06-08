dfb8d5aa67457f703134395d05cad1e939eb0de6
Vandita0307 <parashervandita@gmail.com>
2026-06-08 17:24:56 +0000

    datatypes

---
+++ 01_basics/_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js as newer version
+//alert(3+3) we are using nodejs not browser
+console.log(3+3)
+console.log("Pari")
+
+let name="Pari"
+let age=18
+let isLoggedIn=false
+//number
+//bigint
+//string=>""//''
+//boolean=>true/false
+//null=>standalone value
+//undefined =>variable is declared without value
+//symbol=>unique
+//object
+console.log(typeof "Pari")
+cosole.log(typeof null)//object
