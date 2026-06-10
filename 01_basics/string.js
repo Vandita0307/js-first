15789d4ca0a857f4e679ee89fcd1eec2aea23934
Vandita0307 <parashervandita@gmail.com>
2026-06-10 18:18:47 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,36 @@
+name="Pari";
+repoCount=100;
+//console.log(name+repoCount); OUTDATED SYNTAX
+
+//MODERN DAYS
+//console.log(`Hello I am ${name} and my repocount is ${repoCount}`);
+// Use backticks(``)
+
+const game=new String('Ludo');
+console.log(game);
+
+//console.log(game[0]);//L
+//console.log(game.__proto__);//{}
+//console.log(game.length);//4
+//console.log(game.toUpperCase());//LUDO Calling/executing function
+//console.log(game.toUpperCase);//Not executing just asking to show function
+//console.log(game.charAt(2));//d
+//console.log(game.indexOf('o'));//3
+//console.log(game.indexOf('O'));//-1
+//const news=game.substring(0,2);//Lu
+//console.log(news);
+const gamie='hitesh-hc';
+const another=gamie.slice(-8,4);
+console.log(another);
+const newstring="  hitesh ";
+console.log(newstring.trim());
+const url="https://hitesh.com/hitesh%20choudhry"
+console.log(url.replace('%20','-'));
+console.log(url.includes('hitesh'));
+
+console.log(gamie.split('-'));
+
+
+
+
+
